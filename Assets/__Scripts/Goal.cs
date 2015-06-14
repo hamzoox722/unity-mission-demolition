97a4a035733811b652d1414ee210c2d51eab4d10
JeffreyLee <jeffreyleeon@gmail.com>
2015-06-14 13:05:20 +0800

    Goal set to red when it is hit

--- Assets/__Scripts/Goal.cs
+++ Assets/__Scripts/Goal.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class Goal : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+	// A static field accessible by code anywhere
+	static public bool goalMet = false;
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void OnTriggerEnter( Collider other ) {
+		// When the trigger is hit by something
+		// Check to see if it's a Projectile
+		if (other.gameObject.tag == "Projectile") {
+			// If so, set goalMet to true
+			Goal.goalMet = true;
+			// Also set the alpha of the color to higher opacity
+			Color c = this.renderer.material.color;
+			c.g = 0;
+			c.r = 1;
+			this.renderer.material.color = c;
+		}
 	}
 }
